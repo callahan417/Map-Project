c2947d5ef9859b8b2af0d51b9dfe172e689b46c2
Mike Callahan <callahan417@yahoo.com>
2016-09-02 14:56:01 -0700

    Add functionality to filter list of places

--- geo.js
+++ geo.js
@@ -11,7 +11,7 @@ var data = {
         title: "Eddie McStiff's",
         lat: null,
         lng: null,
-        category: 'Hiking Trail'
+        category: 'Food/Beverage'
     }, {
         title: "Canyonlands National Park",
         lat: null,
@@ -22,25 +22,48 @@ var data = {
 
 //ViewModel
 var ViewModel = function() {
-    this.markers = ko.observableArray();
+    this.places = ko.observableArray();
     data.locations.forEach(function(location) {
-        this.markers.push(location);
+        this.places.push(location);
     }, this);
 
+    //The code below for filtering the this.places() array was adapted from
+    //http://stackoverflow.com/questions/20857594/knockout-filtering-on-observable-array
+    this.currentFilter = ko.observable(); //property to store the filter
+    var self = this;
+    this.filterPlaces = ko.computed(function() {
+        if(!this.currentFilter() || this.currentFilter() === "All") {
+            return this.places();
+        } else {
+            return ko.utils.arrayFilter(this.places(), function(place) {
+                //console.log(self.currentFilter());
+                /*if (place.category === self.currentFilter()) {
+                    place.marker.setMap(map);
+                } else {
+                    place.marker.setMap(null);
+                }*/
+
+                return place.category === self.currentFilter();
+            });
+        }
+    }, this);
+
+    //function that hides/shows list when hamburger icon is clicked
     this.toggleList = function() {
         $('#list-container').toggleClass('hidden');
     };
 
+    //function that changes styling on list item when it is clicked
     this.clickItem = function(data, event) {
-        console.log(data);
+        //console.log(data);
         $(event.target).addClass('highlighted');
         $(event.target).siblings().removeClass('highlighted');
         //console.log(event.target);
-        //TODO: Add code to highlight map marker
+        //TODO: Add code to highlight/animate map marker
     }
 };
 
 var viewModel = new ViewModel();
 ko.applyBindings(viewModel);
 
-console.log(viewModel.markers());
+console.log(viewModel.places());
